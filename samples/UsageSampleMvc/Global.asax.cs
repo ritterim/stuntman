2d6e8c35ba394046b17d09dc6c65b51439da25d8
Ken Dale <ken@kendaleiv.com>
2015-04-23 12:43:45 -0400

    Add UsageSampleMvc

---
+++ samples/UsageSampleMvc/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RimDev.Stuntman.UsageSampleMvc
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
