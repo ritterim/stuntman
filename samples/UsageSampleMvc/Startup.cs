2e35b93c974ca2efc327bedcd33397a8c8eccbbc
Ken Dale <ken@kendaleiv.com>
2015-04-23 12:48:27 -0400

    Add OWIN and UseStuntman

---
+++ samples/UsageSampleMvc/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Owin;
+using RimDev.Stuntman.Core;
+using System.Security.Claims;
+
+namespace RimDev.Stuntman.UsageSampleMvc
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var options = new StuntmanOptions
+            {
+                Users = new[]
+                {
+                    new StuntmanUser
+                    {
+                        Id = "user-1",
+                        Name = "User 1",
+                        Claims = new[]
+                        {
+                            new Claim("given_name", "John"),
+                            new Claim("family_name", "Doe")
+                        }
+                    },
+                }
+            };
+
+            app.UseStuntman(options);
+        }
+    }
+}
