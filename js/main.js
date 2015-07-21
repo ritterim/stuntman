0431928654494ae816a9e21bfae3908e1fe27cb6
Kevin Hougasian <kevin.hougasian@ritterim.com>
2015-07-21 10:49:03 -0400

    scss and js changes

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
-$(document).ready(function($) {
-
+$(document).ready(function() {
+  $('[rel="external"]').attr("target","_blank");
+  
   $(window).bind('scroll', function() {
     ($(window).scrollTop() > 50) ?
       $('#header').addClass('navbar-fixed-top'):
       $('#header').removeClass('navbar-fixed-top');
   });
 
-  $('a.scrollto').on('click', function(e){
-    e.preventDefault();
-
-    var target = this.hash;
-
-    $('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
-    //Collapse mobile menu after clicking
-    if ($('.navbar-collapse').hasClass('in')){
-      $('.navbar-collapse').removeClass('in').addClass('collapse');
-    }
+  $(window).scroll(function() {
+  ($(document).scrollTop() > 500) ?
+    $('#top').show():
+    $('#top').hide();
   });
 
+  $('#top').on('click', function() {
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+  });
+
+  // instead of jquery easing plugin
+  $(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ?
+        target :
+        $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 'slow');
+        return false;
+        }
+      }
+    });
+  });
 });
