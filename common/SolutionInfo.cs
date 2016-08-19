1049d10737b049266304c9e6d44294440b528ff2
Ken Dale <ken@kendaleiv.com>
2016-08-19 10:48:28 -0400

    Update 2015 copyright years to 2016

--- common/SolutionInfo.cs
+++ common/SolutionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyProduct("RimDev.Stuntman")]
-[assembly: AssemblyCopyright("Copyright © 2015 Ritter Insurance Marketing, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2016 Ritter Insurance Marketing, LLC")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
