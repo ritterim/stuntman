0ff2a696e6563c0a2d13aa1b76ec54d7b57da8b9
Sock <sock@andrewlock.net>
2016-09-22 20:00:19 +0100

    Trying to add samples, failed

--- samples/UsageSample/Properties/AssemblyInfo.cs
+++ samples/UsageSample/Properties/AssemblyInfo.cs
@@ -1,4 +1,19 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("RimDev.Stuntman.UsageSample")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UsageSample")]
+[assembly: AssemblyTrademark("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66236b58-0ef2-4484-9257-d957c0f5d71c")]
