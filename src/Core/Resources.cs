d6bfe65e39fab7328a2ef693f4ebe233b10a59f1
Eskild <eskildd@gmail.com>
2017-08-05 10:45:18 +0200

    Cleand up unsued directives

--- src/Core/Resources.cs
+++ src/Core/Resources.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RimDev.Stuntman.Core
+﻿namespace RimDev.Stuntman.Core
 {
     public partial class Resources
     {
