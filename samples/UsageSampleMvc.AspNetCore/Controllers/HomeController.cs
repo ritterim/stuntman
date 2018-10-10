9ef04e9be8faff651ca4e860ce7a1412e1f755c2
Ken Dale <ken@kendaleiv.com>
2018-10-10 15:54:32 -0400

    Add Authorize attributes to verify sample

--- samples/UsageSampleMvc.AspNetCore/Controllers/HomeController.cs
+++ samples/UsageSampleMvc.AspNetCore/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UsageSampleMvc.AspNetCore.Models;
 
@@ -15,6 +12,7 @@ namespace UsageSampleMvc.AspNetCore.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
@@ -22,6 +20,7 @@ namespace UsageSampleMvc.AspNetCore.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
